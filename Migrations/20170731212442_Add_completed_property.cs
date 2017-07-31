e2a528df4980445f47e8c6e3f2f9db5ce2921972
Joshua Fairchild and Olena Kuchko <joshua+olena@>
2017-07-31 14:36:15 -0700

    add lesson + two migrations

---
+++ Migrations/20170731212442_Add_completed_property.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDoListWithMigrations.Migrations
+{
+    public partial class Add_completed_property : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Completed",
+                table: "Items",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Completed",
+                table: "Items");
+        }
+    }
+}
